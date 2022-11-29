c7130feeb670ddc63c3b6645919c4ed5bb8ec35f
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-11-29 01:38:57 +0100

    Add MongoDB

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect('mongodb://admin:password@localhost:27017/articles?authSource=admin', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', async (req, res, next) => {
     res.json({message: 'Hello world'});
 });
-
 
 
 app.listen(PORT, 
