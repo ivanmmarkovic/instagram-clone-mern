851d930a87a261a3593d4d7d7cbdc13a3ac00164
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-12-08 00:39:19 +0100

    Get user by id

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -6,11 +6,28 @@ router
     .route('/users')
         .post(async (req, res, next) => {
             let {username, email, password} = req.body;
-            let user = await UserModel.create({username, email, password});
-            return res.json(user);
+            try {
+                let user = await UserModel.create({username, email, password});
+                return res
+                    .status(201)
+                    .json(user);
+            } catch (error) {
+                return res
+                    .status(400)
+                    .json({message: error.message});
+            }
         })
-        .get((req, res, next) => {
-            res.json({users: []})
+        .get(async (req, res, next) => {
+            let users = await UserModel.find();
+            return res.json(users);
+        });
+
+router
+    .route('/users/:id')
+        .get(async(req, res, next) => {
+            let {id} = req.params;
+            let user = await UserModel.findById(id);
+            return res.json(user);
         });
 
 
