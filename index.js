a5ea09ecfd2ecde93170ecfd708c63a25d071152
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-12-08 00:30:23 +0100

    Create user

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://admin:password@localhost:27017/articles?authSource=admin', {
+mongoose.connect('mongodb://admin:password@localhost:27017/instagram?authSource=admin', {
     useNewUrlParser: true,
     useUnifiedTopology: true 
 });
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const UserModel = require('../models/User');
 
 router
     .route('/users')
+        .post(async (req, res, next) => {
+            let {username, email, password} = req.body;
+            let user = await UserModel.create({username, email, password});
+            return res.json(user);
+        })
         .get((req, res, next) => {
             res.json({users: []})
         });
