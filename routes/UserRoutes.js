895ebb91f271d6bf4ed93bf9a90119d6e1b6f01a
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-12-08 01:09:03 +0100

    Add UserController

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,62 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const UserModel = require('../models/User');
+const UserController = require('../controllers/UserController');
+
 
 router
     .route('/users')
-        .post(async (req, res, next) => {
-            let {username, email, password} = req.body;
-            try {
-                let user = await UserModel.create({username, email, password});
-                return res
-                    .status(201)
-                    .json(user);
-            } catch (error) {
-                return res
-                    .status(400)
-                    .json({message: error.message});
-            }
-        })
-        .get(async (req, res, next) => {
-            let users = await UserModel.find();
-            return res.json(users);
-        });
+        .post(UserController.createUser)
+        .get(UserController.getUsers);
 
 router
     .route('/users/:id')
-        .get(async(req, res, next) => {
-            try {
-                let {id} = req.params;
-                let user = await UserModel.findById(id);
-                return res.json(user);
-            } catch (error) {
-                return res
-                    .status(400)
-                    .json({message: error.message});
-            }
-        })
-        .patch(async(req, res, next) => {
-            try {
-                let {id} = req.params;
-                let user = await UserModel.findByIdAndUpdate(id, {...req.body}, {new: true});
-                return res.json(user);
-            } catch (error) {
-                return res
-                    .status(400)
-                    .json({message: error.message});
-            }
-        })
-        .delete(async(req, res, next) => {
-            try {
-                let {id} = req.params;
-                let user = await UserModel.findByIdAndDelete(id);
-                return res.status(204).json(null);
-            } catch (error) {
-                return res
-                    .status(400)
-                    .json({message: error.message});
-            }
-        });
+        .get(UserController.getUserById)
+        .patch(UserController.patchUser)
+        .delete(UserController.deleteUser);
 
 
 module.exports = router;
