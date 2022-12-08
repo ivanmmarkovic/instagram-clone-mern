52e3c9b62c0829339f38244d8cee3c3a77afae12
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-12-08 01:09:18 +0100

    Create UserController

---
+++ controllers/UserController.js
@@ -0,0 +1,70 @@
+
+
+const UserModel = require('../models/User');
+
+
+const createUser = async (req, res, next) => {
+    let { username, email, password } = req.body;
+    try {
+        let user = await UserModel.create({ username, email, password });
+        return res
+            .status(201)
+            .json(user);
+    } catch (error) {
+        return res
+            .status(400)
+            .json({ message: error.message });
+    }
+};
+
+
+const getUsers = async (req, res, next) => {
+    let users = await UserModel.find();
+    return res.json(users);
+};
+
+
+const getUserById = async(req, res, next) => {
+    try {
+        let {id} = req.params;
+        let user = await UserModel.findById(id);
+        return res.json(user);
+    } catch (error) {
+        return res
+            .status(400)
+            .json({message: error.message});
+    }
+};
+
+const patchUser = async(req, res, next) => {
+    try {
+        let {id} = req.params;
+        let user = await UserModel.findByIdAndUpdate(id, {...req.body}, {new: true});
+        return res.json(user);
+    } catch (error) {
+        return res
+            .status(400)
+            .json({message: error.message});
+    }
+};
+
+const deleteUser = async(req, res, next) => {
+    try {
+        let {id} = req.params;
+        let user = await UserModel.findByIdAndDelete(id);
+        return res.status(204).json(null);
+    } catch (error) {
+        return res
+            .status(400)
+            .json({message: error.message});
+    }
+};
+
+
+module.exports = {
+    createUser,
+    getUsers,
+    getUserById,
+    patchUser,
+    deleteUser
+}
