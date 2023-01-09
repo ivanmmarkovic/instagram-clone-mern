4c16a736b71f2e7e1e32e370fd473e01059ae048
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2023-01-09 23:11:34 +0100

    Handle create story on frontend

--- app/src/consts.js
+++ app/src/consts.js
@@ -2,3 +2,5 @@
 
 export let signup_url = 'http://localhost:5000/users';
 export let login_url = 'http://localhost:5000/login';
+export let create_story_url = 'http://localhost:5000/stories';
+
--- app/src/pages/Stories.js
+++ app/src/pages/Stories.js
@@ -1,10 +1,34 @@
-
+import axios from "axios";
+import { useRef } from "react";
+import {create_story_url} from '../consts';
 
 const Stories = () => {
 
+    const nameInputRef = useRef(null);
+    const fileInputRef = useRef(null);
+
+    const handleCreateStory = () => {
+
+        const formData = new FormData();
+        formData.append('name', nameInputRef.current.value);
+        formData.append('file', fileInputRef.current.value);
+        
+        axios.post(create_story_url, formData, {headers: {
+            'Content-Type': 'multipart/form-data'
+        }})
+        .then(data => console.log(data))
+        .catch(e => console.log(e));
+    };
+
     return (
-        <div>
+        <div className={"stories"}>
             <h1>Stories</h1>
+
+            <input type="text" placeholder="Story name" ref={ nameInputRef }/>
+            <br />
+            <input type="file" name="file" ref={ fileInputRef } />
+            <br />
+            <button onClick={ handleCreateStory }>Create</button>
         </div>
     );
 
