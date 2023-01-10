c16857f1cf5e8511fcd0bcd84c38d94d246a3f37
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2023-01-11 00:28:16 +0100

    Create post finished on backend

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -4,12 +4,16 @@ const PostModel = require('../models/Post');
 const createPost = async (req, res, next) => {
     try {
         console.log('REQ PAYLOAD', req.payload);
+        let {username, id, email} = req.payload;
+
+        let {description} = req.body;
+
         console.log('OTHER FIELD DATA', req.body.description);
         console.log('IMAGE DATA', req.files.file);
 
-        let filepath = path.join(`${__dirname}/../public/${req.files.file.name}`);
+        let filepath = path.join(`${__dirname}/../public/${username}/${req.files.file.name}`);
         await req.files.file.mv(filepath);
-        let post = await PostModel.create({imageUrl: url});
+        let post = await PostModel.create({description, imageUrl: `/${username}/${req.files.file.name}`});
         return res
             .status(201)
             .json({post});
